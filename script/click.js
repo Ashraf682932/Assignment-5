7344df1c3eb9c9f7fb6fef30407c8f43b74a940d
Ashraf Islam <Ashraf682932>
2025-03-30 16:33:41 +0600

    first commit

---
+++ script/click.js
@@ -0,0 +1,38 @@
+document.getElementById('onClick').addEventListener('click',
+    function(){
+        document.getElementById('onClick').style.display ="none"
+        alert()
+    }
+)
+document.getElementById('Click').addEventListener('click',
+    function(){
+         document.getElementById('Click').style.display ="none"
+        alert() 
+    }
+)
+document.getElementById('on-Click').addEventListener('click',
+    function(){
+     document.getElementById('on-Click').style.display ="none"
+        alert()
+    }
+)
+document.getElementById('On-Click').addEventListener('click',
+    function(){
+         document.getElementById('OnClick').style.display ="none"
+        alert()
+    }
+)
+document.getElementById('Click-on').addEventListener('click',
+    function(){
+         document.getElementById('Clic-on').style.display ="none"
+        alert()
+    }
+)
+document.getElementById('Clicked').addEventListener('click',
+    function(){
+         document.getElementById('Clicked').style.display ="none"
+        alert()
+    }
+)
+// const button= document.getElementById('onClick');
+// button.disabled = true;
